5d5d20d1b4d066591a79d6d6fb6fb2afd72851ca
Alex 'AdUser' Z <ad_user@runbox.com>
2015-11-29 19:36:30 +1000

    + add 'referrals' config key

--- src/config.h
+++ src/config.h
@@ -6,6 +6,7 @@ typedef struct {
   char   *binddn;        /** bind as this user before search for user */
   char   *bindpass;      /** bind with this password */
   size_t  bindtimeout;   /** bind timeout */
+  short   referrals;     /** if > 0 - follow referals */
   char   *basedn;        /** where to search for users */
   char   *userfilter;    /** ldap filter for user entry */
   char   *error;         /** parser error */
