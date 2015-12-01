23580c537f460dee394c95be9a6585b7ca6f30f3
Alex 'AdUser' Z <ad_user@runbox.com>
2015-12-01 23:37:55 +1000

    * update header

--- src/ldapauth.h
+++ src/ldapauth.h
@@ -2,8 +2,8 @@
 #define OAL_LDAPAUTH_H_INCLUDED
 
 int
-check_against_ldap(const oal_config_t * const config,
-                   const char * const username,
-                   const char * const password);
+oal_check_cred(const oal_config_t * const config,
+               const char * const username,
+               const char * const password);
 
 #endif /* OAL_LDAPAUTH_H_INCLUDED */
